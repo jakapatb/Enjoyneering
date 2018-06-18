68fdc30a71f0db0f7d7c84de6be93175e3aee601
jakapatb <38038597+jakapatb@users.noreply.github.com>
2018-06-19 03:56:33 +0700

    manage folder

---
+++ index.php
@@ -0,0 +1,26 @@
+<!DOCTYPE html>
+<html lang="en">
+  <head>
+    <meta charset="utf-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1">
+    <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/4.1.0/css/bootstrap.min.css">
+  <script src="https://ajax.googleapis.com/ajax/libs/jquery/3.3.1/jquery.min.js"></script>
+  <script src="https://cdnjs.cloudflare.com/ajax/libs/popper.js/1.14.0/umd/popper.min.js"></script>
+  <script src="https://maxcdn.bootstrapcdn.com/bootstrap/4.1.0/js/bootstrap.min.js"></script>
+  </head>
+
+  <body>
+<div class="container">
+  <div class="row">
+    <div class="col-sm-3 bg-danger">menu1</div>
+    <div class="col-sm-3 bg-info"style="background-color:orange;">menu2</div>
+    <div class="col-sm-3 bg-light" style="background-color:lavender;">menu3</div>
+    <div class="col-sm-3 bg-secondary" style="background-color:orange;">menu4</div>
+  </div>
+
+
+</div>
+
+
+  </body>
+</html>
