b8da505bf80781395f23d5b239f68264a6ea6349
jakapatb <max.jakkapat@gmail.com>
2018-06-27 23:24:19 +0700

    โครงหน้า

--- index.php
+++ index.php
@@ -1,26 +1,51 @@
 <!DOCTYPE html>
 <html lang="en">
-  <head>
+
+<head>
     <meta charset="utf-8">
+    <title>Enjoyneering</title>
     <meta name="viewport" content="width=device-width, initial-scale=1">
     <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/4.1.0/css/bootstrap.min.css">
-  <script src="https://ajax.googleapis.com/ajax/libs/jquery/3.3.1/jquery.min.js"></script>
-  <script src="https://cdnjs.cloudflare.com/ajax/libs/popper.js/1.14.0/umd/popper.min.js"></script>
-  <script src="https://maxcdn.bootstrapcdn.com/bootstrap/4.1.0/js/bootstrap.min.js"></script>
-  </head>
+    <script src="https://ajax.googleapis.com/ajax/libs/jquery/3.3.1/jquery.min.js"></script>
+    <script src="https://cdnjs.cloudflare.com/ajax/libs/popper.js/1.14.0/umd/popper.min.js"></script>
+    <script src="https://maxcdn.bootstrapcdn.com/bootstrap/4.1.0/js/bootstrap.min.js"></script>
+</head>
 
-  <body>
-<div class="container">
-  <div class="row">
-    <div class="col-sm-3 bg-danger">menu1</div>
-    <div class="col-sm-3 bg-info"style="background-color:orange;">menu2</div>
-    <div class="col-sm-3 bg-light" style="background-color:lavender;">menu3</div>
-    <div class="col-sm-3 bg-secondary" style="background-color:orange;">menu4</div>
-  </div>
+<body>
+    <div class="container-fluid">
+        <?php
+         include 'nav.php';
+       ?>
+            <div class="content row col-md-6">
+                <div class="card-deck">
 
+                    <div class="card">
 
-</div>
+                        <img src="img/education.jpg" class="img-fluid" alt="Education">
+                        <div class="card-body">
+                            <h1 class="card-title">Education</h1>
+                            <h5 class="card-subtitle">Education page</h5>
+                            <p>This for Education</p>
+                            <a class="btn btn-dark float-right btn-sm" href="education.php">Education page...</a>
+                        </div>
+                    </div>
 
+                    <div class="card">
 
-  </body>
+                        <img src="img/forum.jpg" class="img-fluid" alt="forum">
+                        <div class="card-body">
+                            <br>
+                            <h1 class="card-title">Forum</h1>
+                            <h5 class="card-subtitle">forum page</h5>
+                            <p>This for forum</p>
+                            <a class="btn btn-dark  btn-sm float-right" href="foum.php">forum page...</a>
+                        </div>
+                    </div>
+
+                </div>
+            </div>
+    </div>
+
+</body>
+
 </html>
---
+++ nav.php
@@ -0,0 +1,14 @@
+<nav class="navbar navbar-expand-md bg-dark navbar-dark ">
+    <a class="navbar-brand" href="index.php">Enjoyneering</a>
+    <button type="button" data-toggle="collapse" data-target="#demo" class="navbar-toggler btn btn-dark border border-secondary btn-sm"><span class="navbar-toggler-icon"></span></button>
+    
+    <div class="collapse navbar-collapse" id="demo">
+    <ul class="navbar-nav " >
+        <li class="nav-item"><a class="nav-link" href="education.php">Education</a></li>
+        <li class="nav-item"><a class="nav-link" href="foum.php">Forum</a></li>
+    </ul>
+     </div>
+   
+
+</nav>
+<br>
