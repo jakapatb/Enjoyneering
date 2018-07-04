341a1228a506864f8c4eb0148ab069baf065b50b
jakapatb <max.jakkapat@gmail.com>
2018-07-04 14:02:12 +0700

    footer หน้าแรกเปลี่ยนใหม่

--- index.php
+++ index.php
@@ -9,54 +9,163 @@
     <script src="https://ajax.googleapis.com/ajax/libs/jquery/3.3.1/jquery.min.js"></script>
     <script src="https://cdnjs.cloudflare.com/ajax/libs/popper.js/1.14.0/umd/popper.min.js"></script>
     <script src="https://maxcdn.bootstrapcdn.com/bootstrap/4.1.0/js/bootstrap.min.js"></script>
+</head>
 
-
-    <body>
-        <?php
+<body>
+    <?php
          include 'nav.php';
        ?>
-            <div class="container-fluid">
-                <!-- Top menu bar -->
-
-                <!-- Card frame -->
-                <div class="content row col-md-6 d-flex justify-content-center">
-                    <div class="card-deck">
-                        <!--Card:Education -->
-                        <div class="card">
-                            <img src="img/education.jpg" class="img-fluid" alt="Education">
-
-
-                            <div class="card-body">
-                                <h1 class="card-title">Education</h1>
-                                <h5 class="card-subtitle">Education page</h5>
-                                <p>This for Education</p>
-
+        <div class="container">
+            <!--left column-->
+            <div class="row bg-light">
+                <div class="col col-md-8" >
+                    <!-- Top slide  -->
+                    <div id="topCarousel" class="carousel slide " data-ride="carousel">
+                        <!-- Indicators -->
+                        <ul class="carousel-indicators">
+                            <li data-target="#topCarousel" data-slide-to="0" class="active"></li>
+                            <li data-target="#topCarousel" data-slide="1"></li>
+                        </ul>
+                        <!-- The slideshow -->
+                        <div class="carousel-inner">
+                            <div class="carousel-item active">
+                                <img src="img/test2.jpg" class=" img-responsive img-fluid" alt="test2">
                             </div>
-
-                            <div class="card-footer"><a class="btn btn-dark float-right btn-sm" href="education.php">Education page...</a></div>
-                        </div>
-                        <!--Card:Forum -->
-                        <div class="card">
-
-                            <img src="img/forum.jpg" class="img-fluid" alt="forum">
-                            <div class="card-body">
-                                <br>
-                                <h1 class="card-title">Forum</h1>
-                                <h5 class="card-subtitle">forum page</h5>
-                                <p>This for forum</p>
+                            <div class="carousel-item ">
+                                <img src="img/test1.jpg" class=" img-responsive img-fluid" alt="test1">
                             </div>
-                            <div class="card-footer"> <a class="btn btn-dark  btn-sm float-right" href="foum.php">forum page...</a> </div>
-
                         </div>
+
+                        <!-- Left and right controls -->
+                        <a class="carousel-control-prev" href="#topCarousel" data-slide="prev">
+       <span class="carousel-control-prev-icon"></span>
+   </a>
+                        <a class="carousel-control-next" href="#topCarousel" data-slide="next">
+       <span class="carousel-control-next-icon"></span>
+   </a>
 
                     </div>
+                    <br>
+                    <!-- Top topics: -->
+                    <div class="d-flex flex-row bg-dark">
+                        <div class="p-2 bg-warning"> Enjoyneering Forum</div>
+                    </div>
+
+
+                    <!-- Card frame -->
+                    <div class="content">
+                        <br>
+                        <div class="card-columns">
+                            <!--Card:Education -->
+                            <div class="card">
+                                <img src="img/education.jpg" class="img-fluid" alt="Education">
+
+
+                                <div class="card-body">
+                                    <h1 class="card-title">Education</h1>
+                                    <h5 class="card-subtitle">Education page</h5>
+                                    <p>This for Education</p>
+
+                                </div>
+
+                                <div class="card-footer"><a class="btn btn-dark float-right btn-sm" href="education.php">Education page...</a></div>
+                            </div>
+                            <!--Card:Forum -->
+                            <div class="card">
+
+                                <img src="img/forum.jpg" class="img-fluid" alt="forum">
+                                <div class="card-body">
+                                    <br>
+                                    <h1 class="card-title">Forum</h1>
+                                    <h5 class="card-subtitle">forum page</h5>
+                                    <p>This for forum</p>
+                                </div>
+                                <div class="card-footer"> <a class="btn btn-dark  btn-sm float-right" href="foum.php">forum page...</a> </div>
+
+                            </div>
+                            <!--Card:Forum -->
+                            <div class="card">
+
+                                <img src="img/forum.jpg" class="img-fluid" alt="forum">
+                                <div class="card-body">
+                                    <br>
+                                    <h1 class="card-title">Forum</h1>
+                                    <h5 class="card-subtitle">forum page</h5>
+                                    <p>This for forum</p>
+                                </div>
+                                <div class="card-footer"> <a class="btn btn-dark  btn-sm float-right" href="foum.php">forum page...</a> </div>
+
+                            </div>
+
+                        </div>
+                    </div>
+
+
+                    <!-- Top topics: -->
+                    <div class="d-flex flex-row bg-dark">
+                        <div class="p-2 bg-warning"> Enjoyneering Forum</div>
+                    </div>
+                    <!-- Card frame -->
+                    <div class="content">
+                        <br>
+                        <div class="card-columns">
+                            <!--Card:Education -->
+                            <div class="card">
+                                <img src="img/education.jpg" class="img-fluid" alt="Education">
+
+
+                                <div class="card-body">
+                                    <h1 class="card-title">Education</h1>
+                                    <h5 class="card-subtitle">Education page</h5>
+                                    <p>This for Education</p>
+
+                                </div>
+
+                                <div class="card-footer"><a class="btn btn-dark float-right btn-sm" href="education.php">Education page...</a></div>
+                            </div>
+                            <!--Card:Forum -->
+                            <div class="card">
+
+                                <img src="img/forum.jpg" class="img-fluid" alt="forum">
+                                <div class="card-body">
+                                    <br>
+                                    <h1 class="card-title">Forum</h1>
+                                    <h5 class="card-subtitle">forum page</h5>
+                                    <p>This for forum</p>
+                                </div>
+                                <div class="card-footer"> <a class="btn btn-dark  btn-sm float-right" href="foum.php">forum page...</a> </div>
+
+                            </div>
+                            <!--Card:Forum -->
+                            <div class="card">
+
+                                <img src="img/forum.jpg" class="img-fluid" alt="forum">
+                                <div class="card-body">
+                                    <br>
+                                    <h1 class="card-title">Forum</h1>
+                                    <h5 class="card-subtitle">forum page</h5>
+                                    <p>This for forum</p>
+                                </div>
+                                <div class="card-footer"> <a class="btn btn-dark  btn-sm float-right" href="foum.php">forum page...</a> </div>
+
+                            </div>
+
+                        </div>
+                    </div>
+
+
                 </div>
+
+<!-- Right columns-->
+                <div class="col col-xl-4 bg-dark"></div>
             </div>
-            <?php
+        </div>
+        
+    <?php
     
 include 'footer.php';
     
 ?>
-    </body>
+</body>
 
 </html>
