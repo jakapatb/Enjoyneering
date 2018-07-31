7b82aef1d2c8d0cdbaa5718ae155d0a93d464c36
0Empty0 <59010679@kmitl.ac.th>
2018-07-31 21:10:45 +0700

    ลบไฟล์ที่ไม่จำเป็น

--- src/config/action_login.js
+++
@@ -1,2 +0,0 @@
-/*TODO : creat new php page(replace #goToSign) for login */
-//โค้ดส่วนของ Flie base
--- src/config/action_logout.js
+++
@@ -1,2 +0,0 @@
-/*TODO : creat new php page(replace #goToSign) for logout */
-//โค้ดส่วนของ Flie base
